0cd442ecfacf572664bfb2912362810792f4f5d4
aranya170 <aranya.akd@gmail.com>
2025-05-13 19:46:11 +0600

    Initial Push

--- includes/Footer.php
+++ includes/Footer.php
@@ -1,108 +1,93 @@
-<!DOCTYPE html>
-<html lang="en">
-
-<head>
-    <meta charset="UTF-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Document</title>
-    <script src="https://cdn.tailwindcss.com"></script>
-</head>
-
-<body>
-    <footer class="relative text-white bg-[#262626] lg:bg-transparent h-auto lg:h-[494px]">
-        <!-- Background SVG (Hidden in mobile view) -->
-        <svg xmlns="http://www.w3.org/2000/svg" width="100%" height="494" fill="none" preserveAspectRatio="none"
-            viewBox="0 0 1435 494" class="sm:hidden xs:hidden lg:block absolute bottom-0 left-0 m-[15px] -z-10">
-            <path fill="#262626"
-                d="M0 10C0 4.477 4.477 0 10 0h363.802a9.999 9.999 0 018.461 4.67l24.992 39.668a9.998 9.998 0 008.46 4.67h828.325c3.51 0 6.77 1.843 8.58 4.855l17.69 29.496a10.015 10.015 0 008.58 4.855h122.54c5.52 0 10 4.477 10 10V484c0 5.523-4.48 10-10 10H10c-5.523 0-10-4.477-10-10V10z">
-            </path>
-        </svg>
-        <!-- Nut SVG positioned in top right corner (Hidden in mobile view) -->
-        <svg fill="#000000" width="15px" height="15px" class="absolute top-[85px] right-[25px] z-10 lg:block hidden"
-            viewBox="0 0 24 24" id="nut-2" data-name="Flat Color" xmlns="http://www.w3.org/2000/svg"
-            class="icon flat-color">
-            <path id="primary"
-                d="M20,6.2,13,2.26a2,2,0,0,0-2,0L4,6.19A2,2,0,0,0,3,7.94v8.12A2,2,0,0,0,4,17.8l7,3.94a2,2,0,0,0,2,0l7-3.93a2,2,0,0,0,1-1.75V7.94A2,2,0,0,0,20,6.2ZM12,15a3,3,0,1,1,3-3A3,3,0,0,1,12,15Z"
-                style="fill: #fff;"></path>
-        </svg>
-        <svg fill="#000000" width="15px" height="15px" class="absolute bottom-[25px] right-[25px] z-10 lg:block hidden"
-            viewBox="0 0 24 24" id="nut-2" data-name="Flat Color" xmlns="http://www.w3.org/2000/svg"
-            class="icon flat-color">
-            <path id="primary"
-                d="M20,6.2,13,2.26a2,2,0,0,0-2,0L4,6.19A2,2,0,0,0,3,7.94v8.12A2,2,0,0,0,4,17.8l7,3.94a2,2,0,0,0,2,0l7-3.93a2,2,0,0,0,1-1.75V7.94A2,2,0,0,0,20,6.2ZM12,15a3,3,0,1,1,3-3A3,3,0,0,1,12,15Z"
-                style="fill: #fff;"></path>
-        </svg>
-        <svg fill="#000000" width="15px" height="15px" class="absolute bottom-[25px] left-[25px] z-10 lg:block hidden"
-            viewBox="0 0 24 24" id="nut-2" data-name="Flat Color" xmlns="http://www.w3.org/2000/svg"
-            class="icon flat-color">
-            <path id="primary"
-                d="M20,6.2,13,2.26a2,2,0,0,0-2,0L4,6.19A2,2,0,0,0,3,7.94v8.12A2,2,0,0,0,4,17.8l7,3.94a2,2,0,0,0,2,0l7-3.93a2,2,0,0,0,1-1.75V7.94A2,2,0,0,0,20,6.2ZM12,15a3,3,0,1,1,3-3A3,3,0,0,1,12,15Z"
-                style="fill: #fff;"></path>
-        </svg>
-        <svg fill="#000000" width="15px" height="15px" class="absolute top-[5px] left-[25px] z-10 lg:block hidden"
-            viewBox="0 0 24 24" id="nut-2" data-name="Flat Color" xmlns="http://www.w3.org/2000/svg"
-            class="icon flat-color">
-            <path id="primary"
-                d="M20,6.2,13,2.26a2,2,0,0,0-2,0L4,6.19A2,2,0,0,0,3,7.94v8.12A2,2,0,0,0,4,17.8l7,3.94a2,2,0,0,0,2,0l7-3.93a2,2,0,0,0,1-1.75V7.94A2,2,0,0,0,20,6.2ZM12,15a3,3,0,1,1,3-3A3,3,0,0,1,12,15Z"
-                style="fill: #fff;"></path>
-        </svg>
-        <div class="container max-w-7xl mx-auto px-4 sm:px-6 lg:px-16 pt-12 sm:pt-24 pb-8 sm:pb-12 relative z-10">
-            <div class="grid grid-cols-1 sm:grid-cols-2 md:grid-cols-3 gap-6 sm:gap-8">
-                <!-- Club Info -->
-                <div>
-                    <img src="includes/Icon.png" class="h-16 w-16 sm:h-20 sm:w-20 mx-auto sm:mx-0"
-                        alt="UIU Robotics Club Logo" srcset="">
-                    <h3 class="text-xl sm:text-2xl font-bold mb-4 text-center sm:text-left">UIU Robotics Club</h3>
-                    <p class="text-gray-300 text-sm sm:text-base text-center sm:text-left">Fostering innovation and
-                        creativity through robotics and technology at United International University.</p>
-                </div>
-                <!-- Quick Links -->
-                <div>
-                    <h3 class="text-lg sm:text-xl font-semibold mb-4 text-center sm:text-left">Quick Links</h3>
-                    <ul class="space-y-2 text-sm sm:text-base text-center sm:text-left">
-                        <li><a href="#" class="hover:text-blue-400 transition">Home</a></li>
-                        <li><a href="#" class="hover:text-blue-400 transition">About Us</a></li>
-                        <li><a href="#" class="hover:text-blue-400 transition">Projects</a></li>
-                        <li><a href="#" class="hover:text-blue-400 transition">Events</a></li>
-                        <li><a href="#" class="hover:text-blue-400 transition">Contact</a></li>
-                    </ul>
-                </div>
-                <!-- Contact Info -->
-                <div>
-                    <h3 class="text-lg sm:text-xl font-semibold mb-4 text-center sm:text-left">Contact Us</h3>
-                    <p class="text-gray-300 text-sm sm:text-base text-center sm:text-left">Email: <a
-                            href="mailto:robotics@uiu.ac.bd"
-                            class="hover:text-blue-400 transition">robotics@uiu.ac.bd</a>
-                    </p>
-                    <p class="text-gray-300 text-sm sm:text-base text-center sm:text-left">Phone: +880 1234-567890</p>
-                    <p class="text-gray-300 text-sm sm:text-base text-center sm:text-left">Address: United International
-                        University, Dhaka, Bangladesh</p>
-                    <div class="flex space-x-4 mt-4 justify-center sm:justify-start">
-                        <a href="#" class="hover:text-blue-400 transition">
-                            <svg class="w-5 h-5 sm:w-6 sm:h-6" fill="currentColor" viewBox="0 0 24 24">
-                                <path
-                                    d="M12 2C6.48 2 2 6.48 2 12s4.48 10 10 10 10-4.48 10-10S17.52 2 12 2zm-1 17.93c-3.95-.49-7-3.85-7-7.93 0-.62.08-1.21.22-1.79L9 14v1c0 1.1.9 2 2 2v1.93zm6.9-2.54c-.26-.81-1-1.39-1.9-1.39h-1v-3c0-.55-.45-1-1-1h-6v-2h2c.55 0 1-.45 1-1V7h2c1.1 0 2-.9 2-2v-.41c2.93 1.19 5 4.06 5 7.41 0 2.08-.8 3.97-2.1 5.39z" />
-                            </svg>
-                        </a>
-                        <a href="#" class="hover:text-blue-400 transition"><svg class="w-5 h-5 sm:w-6 sm:h-6"
-                                fill="currentColor" viewBox="0 0 24 24">
-                                <path
-                                    d="M22 12c0-5.52-4.48-10-10-10S2 6.48 2 12c0 4.84 3.44 8.87 8 9.8V15H7v-3h3V9c0-2.76 1.24-5 3-5h3v3h-3c-.55 0-1 .45-1 1v3h4l-.6 3H14v6.8c4.56-.93 8-4.96 8-9.8z" />
-                            </svg></a>
-                        <a href="#" class="hover:text-blue-400 transition"><svg class="w-5 h-5 sm:w-6 sm:h-6"
-                                fill="currentColor" viewBox="0 0 24 24">
-                                <path
-                                    d="M7.8 2h8.4C19.4 2 22 4.6 22 7.8v8.4c0 3.2-2.6 5.8-5.8 5.8H7.8C4.6 22 2 19.4 2 16.2V7.8C2 4.6 4.6 2 7.8 2zm-.2 2C5.6 4 4 5.6 4 7.6v8.8c0 2 1.6 3.6 3.6 3.6h8.8c2 0 3.6-1.6 3.6-3.6V7.6c0-2-1.6-3.6-3.6-3.6H7.6zm9.6 2c1.1 0 2 .9 2 2s-.9 2-2 2-2-.9-2-2 .9-2 2-2zm-9.6 12h8c.6 0 1-.4 1-1s-.4-1-1-1h-8c-.6 0-1 .4-1 1s.4 1 1 1z" />
-                            </svg></a>
-                    </div>
-                </div>
+<footer class="relative text-white bg-[#262626] lg:bg-transparent h-auto lg:h-[494px]">
+    <!-- Background SVG (Hidden in mobile view) -->
+    <svg xmlns="http://www.w3.org/2000/svg" width="100%" height="494" fill="none" preserveAspectRatio="none"
+        viewBox="0 0 1435 494" class="sm:hidden xs:hidden lg:block absolute bottom-0 left-0 m-[15px] -z-10">
+        <path fill="#262626"
+            d="M0 10C0 4.477 4.477 0 10 0h363.802a9.999 9.999 0 018.461 4.67l24.992 39.668a9.998 9.998 0 008.46 4.67h828.325c3.51 0 6.77 1.843 8.58 4.855l17.69 29.496a10.015 10.015 0 008.58 4.855h122.54c5.52 0 10 4.477 10 10V484c0 5.523-4.48 10-10 10H10c-5.523 0-10-4.477-10-10V10z">
+        </path>
+    </svg>
+    <!-- Nut SVG positioned in top right corner (Hidden in mobile view) -->
+    <svg fill="#000000" width="15px" height="15px" class="absolute top-[85px] right-[25px] z-10 lg:block hidden"
+        viewBox="0 0 24 24" id="nut-2" data-name="Flat Color" xmlns="http://www.w3.org/2000/svg"
+        class="icon flat-color">
+        <path id="primary"
+            d="M20,6.2,13,2.26a2,2,0,0,0-2,0L4,6.19A2,2,0,0,0,3,7.94v8.12A2,2,0,0,0,4,17.8l7,3.94a2,2,0,0,0,2,0l7-3.93a2,2,0,0,0,1-1.75V7.94A2,2,0,0,0,20,6.2ZM12,15a3,3,0,1,1,3-3A3,3,0,0,1,12,15Z"
+            style="fill: #fff;"></path>
+    </svg>
+    <svg fill="#000000" width="15px" height="15px" class="absolute bottom-[25px] right-[25px] z-10 lg:block hidden"
+        viewBox="0 0 24 24" id="nut-2" data-name="Flat Color" xmlns="http://www.w3.org/2000/svg"
+        class="icon flat-color">
+        <path id="primary"
+            d="M20,6.2,13,2.26a2,2,0,0,0-2,0L4,6.19A2,2,0,0,0,3,7.94v8.12A2,2,0,0,0,4,17.8l7,3.94a2,2,0,0,0,2,0l7-3.93a2,2,0,0,0,1-1.75V7.94A2,2,0,0,0,20,6.2ZM12,15a3,3,0,1,1,3-3A3,3,0,0,1,12,15Z"
+            style="fill: #fff;"></path>
+    </svg>
+    <svg fill="#000000" width="15px" height="15px" class="absolute bottom-[25px] left-[25px] z-10 lg:block hidden"
+        viewBox="0 0 24 24" id="nut-2" data-name="Flat Color" xmlns="http://www.w3.org/2000/svg"
+        class="icon flat-color">
+        <path id="primary"
+            d="M20,6.2,13,2.26a2,2,0,0,0-2,0L4,6.19A2,2,0,0,0,3,7.94v8.12A2,2,0,0,0,4,17.8l7,3.94a2,2,0,0,0,2,0l7-3.93a2,2,0,0,0,1-1.75V7.94A2,2,0,0,0,20,6.2ZM12,15a3,3,0,1,1,3-3A3,3,0,0,1,12,15Z"
+            style="fill: #fff;"></path>
+    </svg>
+    <svg fill="#000000" width="15px" height="15px" class="absolute top-[5px] left-[25px] z-10 lg:block hidden"
+        viewBox="0 0 24 24" id="nut-2" data-name="Flat Color" xmlns="http://www.w3.org/2000/svg"
+        class="icon flat-color">
+        <path id="primary"
+            d="M20,6.2,13,2.26a2,2,0,0,0-2,0L4,6.19A2,2,0,0,0,3,7.94v8.12A2,2,0,0,0,4,17.8l7,3.94a2,2,0,0,0,2,0l7-3.93a2,2,0,0,0,1-1.75V7.94A2,2,0,0,0,20,6.2ZM12,15a3,3,0,1,1,3-3A3,3,0,0,1,12,15Z"
+            style="fill: #fff;"></path>
+    </svg>
+    <div class="container max-w-7xl mx-auto px-4 sm:px-6 lg:px-16 pt-12 sm:pt-24 pb-8 sm:pb-12 relative z-10">
+        <div class="grid grid-cols-1 sm:grid-cols-2 md:grid-cols-3 gap-6 sm:gap-8">
+            <!-- Club Info -->
+            <div>
+                <img src="includes/Icon.png" class="h-16 w-16 sm:h-20 sm:w-20 mx-auto sm:mx-0"
+                    alt="UIU Robotics Club Logo" srcset="">
+                <h3 class="text-xl sm:text-2xl font-bold mb-4 text-center sm:text-left">UIU Robotics Club</h3>
+                <p class="text-gray-300 text-sm sm:text-base text-center sm:text-left">Fostering innovation and
+                    creativity through robotics and technology at United International University.</p>
             </div>
-            <!-- Bottom Bar -->
-            <div class="mt-6 sm:mt-8 pt-6 sm:pt-8 border-t border-white-700 text-center">
-                <p class="text-white-400 text-sm sm:text-base">© 2025 UIU Robotics Club. All rights reserved.</p>
+            <!-- Quick Links -->
+            <div>
+                <h3 class="text-lg sm:text-xl font-semibold mb-4 text-center sm:text-left">Quick Links</h3>
+                <ul class="space-y-2 text-sm sm:text-base text-center sm:text-left">
+                    <li><a href="#" class="hover:text-blue-400 transition">Home</a></li>
+                    <li><a href="#" class="hover:text-blue-400 transition">About Us</a></li>
+                    <li><a href="#" class="hover:text-blue-400 transition">Projects</a></li>
+                    <li><a href="#" class="hover:text-blue-400 transition">Events</a></li>
+                    <li><a href="#" class="hover:text-blue-400 transition">Contact</a></li>
+                </ul>
+            </div>
+            <!-- Contact Info -->
+            <div>
+                <h3 class="text-lg sm:text-xl font-semibold mb-4 text-center sm:text-left">Contact Us</h3>
+                <p class="text-gray-300 text-sm sm:text-base text-center sm:text-left">Email: <a
+                        href="mailto:robotics@uiu.ac.bd" class="hover:text-blue-400 transition">robotics@uiu.ac.bd</a>
+                </p>
+                <p class="text-gray-300 text-sm sm:text-base text-center sm:text-left">Phone: +880 1234-567890</p>
+                <p class="text-gray-300 text-sm sm:text-base text-center sm:text-left">Address: United International
+                    University, Dhaka, Bangladesh</p>
+                <div class="flex space-x-4 mt-4 justify-center sm:justify-start">
+                    <a href="#" class="hover:text-blue-400 transition">
+                        <svg class="w-5 h-5 sm:w-6 sm:h-6" fill="currentColor" viewBox="0 0 24 24">
+                            <path
+                                d="M12 2C6.48 2 2 6.48 2 12s4.48 10 10 10 10-4.48 10-10S17.52 2 12 2zm-1 17.93c-3.95-.49-7-3.85-7-7.93 0-.62.08-1.21.22-1.79L9 14v1c0 1.1.9 2 2 2v1.93zm6.9-2.54c-.26-.81-1-1.39-1.9-1.39h-1v-3c0-.55-.45-1-1-1h-6v-2h2c.55 0 1-.45 1-1V7h2c1.1 0 2-.9 2-2v-.41c2.93 1.19 5 4.06 5 7.41 0 2.08-.8 3.97-2.1 5.39z" />
+                        </svg>
+                    </a>
+                    <a href="#" class="hover:text-blue-400 transition"><svg class="w-5 h-5 sm:w-6 sm:h-6"
+                            fill="currentColor" viewBox="0 0 24 24">
+                            <path
+                                d="M22 12c0-5.52-4.48-10-10-10S2 6.48 2 12c0 4.84 3.44 8.87 8 9.8V15H7v-3h3V9c0-2.76 1.24-5 3-5h3v3h-3c-.55 0-1 .45-1 1v3h4l-.6 3H14v6.8c4.56-.93 8-4.96 8-9.8z" />
+                        </svg></a>
+                    <a href="#" class="hover:text-blue-400 transition"><svg class="w-5 h-5 sm:w-6 sm:h-6"
+                            fill="currentColor" viewBox="0 0 24 24">
+                            <path
+                                d="M7.8 2h8.4C19.4 2 22 4.6 22 7.8v8.4c0 3.2-2.6 5.8-5.8 5.8H7.8C4.6 22 2 19.4 2 16.2V7.8C2 4.6 4.6 2 7.8 2zm-.2 2C5.6 4 4 5.6 4 7.6v8.8c0 2 1.6 3.6 3.6 3.6h8.8c2 0 3.6-1.6 3.6-3.6V7.6c0-2-1.6-3.6-3.6-3.6H7.6zm9.6 2c1.1 0 2 .9 2 2s-.9 2-2 2-2-.9-2-2 .9-2 2-2zm-9.6 12h8c.6 0 1-.4 1-1s-.4-1-1-1h-8c-.6 0-1 .4-1 1s.4 1 1 1z" />
+                        </svg></a>
+                </div>
             </div>
         </div>
-    </footer>
-</body>
-
-</html>
+        <!-- Bottom Bar -->
+        <div class="mt-6 sm:mt-8 pt-6 sm:pt-8 border-t border-white-700 text-center">
+            <p class="text-white-400 text-sm sm:text-base">© 2025 UIU Robotics Club. All rights reserved.</p>
+        </div>
+    </div>
+</footer>
--- includes/Navbar.php
+++ includes/Navbar.php
@@ -1,13 +1,4 @@
-<!DOCTYPE html>
-<html lang="en">
-
-<head>
-    <meta charset="UTF-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Document</title>
-    <script src="https://cdn.tailwindcss.com"></script>
-    <link href="https://fonts.googleapis.com/css2?family=Exo+2:wght@400;600;700&display=swap" rel="stylesheet">
-    <style>
+<style>
     body {
         font-family: 'Exo 2', sans-serif;
     }
@@ -27,110 +18,105 @@
     .main-btn:focus {
         outline: none;
     }
-    </style>
-</head>
-
-<body>
-    <nav class="w-[625px] h-[143px] flex-shrink-0 p-0 m-0" style="z-index: 20;">
-        <svg xmlns="http://www.w3.org/2000/svg" class="absolute top-0 left-0" width="625" height="143" fill="none"
-            viewBox="0 0 625 143">
-            <path fill="#262626"
-                d="M625 0v79.396H304.73a61.195 61.195 0 01-.844.557c-8.001 5.649-25.531 18.001-31.642 22.269-3.847 2.682-5.59 5.761-6.26 8.197v15.621A15.896 15.896 0 01250 142.366H0V0h625z">
-            </path>
-        </svg>
-        <hr class="w-full -z-10 bg-black h-[2px] absolute top-[78.5px] -translate-y-[1px]">
-        <svg fill="#000000" width="15px" height="15px" class="absolute top-[10px] left-[10px] z-10" viewBox="0 0 24 24"
-            id="nut-2" data-name="Flat Color" xmlns="http://www.w3.org/2000/svg" class="icon flat-color">
-            <path id="primary"
-                d="M20,6.2,13,2.26a2,2,0,0,0-2,0L4,6.19A2,2,0,0,0,3,7.94v8.12A2,2,0,0,0,4,17.8l7,3.94a2,2,0,0,0,2,0l7-3.93a2,2,0,0,0,1-1.75V7.94A2,2,0,0,0,20,6.2ZM12,15a3,3,0,1,1,3-3A3,3,0,0,1,12,15Z"
-                style="fill: #fff;"></path>
-        </svg>
-        <svg fill="#000000" width="15px" height="15px" class="absolute top-[110px] left-[10px] z-10" viewBox="0 0 24 24"
-            id="nut-2" data-name="Flat Color" xmlns="http://www.w3.org/2000/svg" class="icon flat-color">
-            <path id="primary"
-                d="M20,6.2,13,2.26a2,2,0,0,0-2,0L4,6.19A2,2,0,0,0,3,7.94v8.12A2,2,0,0,0,4,17.8l7,3.94a2,2,0,0,0,2,0l7-3.93a2,2,0,0,0,1-1.75V7.94A2,2,0,0,0,20,6.2ZM12,15a3,3,0,1,1,3-3A3,3,0,0,1,12,15Z"
-                style="fill: #fff;"></path>
-        </svg>
-        <svg fill="#000000" width="15px" height="15px" class="absolute top-[110px] left-[240px] z-10"
-            viewBox="0 0 24 24" id="nut-2" data-name="Flat Color" xmlns="http://www.w3.org/2000/svg"
-            class="icon flat-color">
-            <path id="primary"
-                d="M20,6.2,13,2.26a2,2,0,0,0-2,0L4,6.19A2,2,0,0,0,3,7.94v8.12A2,2,0,0,0,4,17.8l7,3.94a2,2,0,0,0,2,0l7-3.93a2,2,0,0,0,1-1.75V7.94A2,2,0,0,0,20,6.2ZM12,15a3,3,0,1,1,3-3A3,3,0,0,1,12,15Z"
-                style="fill: #fff;"></path>
-        </svg>
-        <svg fill="#000000" width="15px" height="15px" class="absolute top-[10px] left-[600px] z-10" viewBox="0 0 24 24"
-            id="nut-2" data-name="Flat Color" xmlns="http://www.w3.org/2000/svg" class="icon flat-color">
-            <path id="primary"
-                d="M20,6.2,13,2.26a2,2,0,0,0-2,0L4,6.19A2,2,0,0,0,3,7.94v8.12A2,2,0,0,0,4,17.8l7,3.94a2,2,0,0,0,2,0l7-3.93a2,2,0,0,0,1-1.75V7.94A2,2,0,0,0,20,6.2ZM12,15a3,3,0,1,1,3-3A3,3,0,0,1,12,15Z"
-                style="fill: #fff;"></path>
-        </svg>
-        <svg fill="#000000" width="15px" height="15px" class="absolute top-[55px] left-[600px] z-10" viewBox="0 0 24 24"
-            id="nut-2" data-name="Flat Color" xmlns="http://www.w3.org/2000/svg" class="icon flat-color">
-            <path id="primary"
-                d="M20,6.2,13,2.26a2,2,0,0,0-2,0L4,6.19A2,2,0,0,0,3,7.94v8.12A2,2,0,0,0,4,17.8l7,3.94a2,2,0,0,0,2,0l7-3.93a2,2,0,0,0,1-1.75V7.94A2,2,0,0,0,20,6.2ZM12,15a3,3,0,1,1,3-3A3,3,0,0,1,12,15Z"
-                style="fill: #fff;"></path>
-        </svg>
-        <img src="includes/Icon.png" class="h-20 w-20 absolute top-[25px] left-[70px] z-10" alt="">
-    </nav>
-    <!-- Full Menu (visible when viewport > 1022px) -->
-    <div id="fullMenu" class="hidden lg:flex items-center justify-center gap-[30px] absolute top-[25px] right-10">
-        <a href=""
-            class="text-[#262626] hover:text-[#ff0302] text-lg font-medium relative transition-colors duration-300 group">
-            Home
-        </a>
-        <a href=""
-            class="text-[#262626] hover:text-[#ff0302] text-lg font-medium relative transition-colors duration-300 group">
-            About Us
-        </a>
-        <a href=""
-            class="text-[#262626] hover:text-[#ff0302] text-lg font-medium relative transition-colors duration-300 group">
-            Events
-        </a>
-        <a href=""
-            class="text-[#262626] hover:text-[#ff0302] text-lg font-medium relative transition-colors duration-300 group">
-            Achievements
-        </a>
-        <a href=""
-            class="text-[#262626] hover:text-[#ff0302] text-lg font-medium relative transition-colors duration-300 group">
-            Contact Us
-        </a>
+</style>
+<nav class="w-[625px] h-[143px] flex-shrink-0 p-0 m-0" style="z-index: 20;">
+    <svg xmlns="http://www.w3.org/2000/svg" class="absolute top-0 left-0" width="625" height="143" fill="none"
+        viewBox="0 0 625 143">
+        <path fill="#262626"
+            d="M625 0v79.396H304.73a61.195 61.195 0 01-.844.557c-8.001 5.649-25.531 18.001-31.642 22.269-3.847 2.682-5.59 5.761-6.26 8.197v15.621A15.896 15.896 0 01250 142.366H0V0h625z">
+        </path>
+    </svg>
+    <hr class="w-full -z-10 bg-black h-[2px] absolute top-[78.5px] -translate-y-[1px]">
+    <svg fill="#000000" width="15px" height="15px" class="absolute top-[10px] left-[10px] z-10" viewBox="0 0 24 24"
+        id="nut-2" data-name="Flat Color" xmlns="http://www.w3.org/2000/svg" class="icon flat-color">
+        <path id="primary"
+            d="M20,6.2,13,2.26a2,2,0,0,0-2,0L4,6.19A2,2,0,0,0,3,7.94v8.12A2,2,0,0,0,4,17.8l7,3.94a2,2,0,0,0,2,0l7-3.93a2,2,0,0,0,1-1.75V7.94A2,2,0,0,0,20,6.2ZM12,15a3,3,0,1,1,3-3A3,3,0,0,1,12,15Z"
+            style="fill: #fff;"></path>
+    </svg>
+    <svg fill="#000000" width="15px" height="15px" class="absolute top-[110px] left-[10px] z-10" viewBox="0 0 24 24"
+        id="nut-2" data-name="Flat Color" xmlns="http://www.w3.org/2000/svg" class="icon flat-color">
+        <path id="primary"
+            d="M20,6.2,13,2.26a2,2,0,0,0-2,0L4,6.19A2,2,0,0,0,3,7.94v8.12A2,2,0,0,0,4,17.8l7,3.94a2,2,0,0,0,2,0l7-3.93a2,2,0,0,0,1-1.75V7.94A2,2,0,0,0,20,6.2ZM12,15a3,3,0,1,1,3-3A3,3,0,0,1,12,15Z"
+            style="fill: #fff;"></path>
+    </svg>
+    <svg fill="#000000" width="15px" height="15px" class="absolute top-[110px] left-[240px] z-10" viewBox="0 0 24 24"
+        id="nut-2" data-name="Flat Color" xmlns="http://www.w3.org/2000/svg" class="icon flat-color">
+        <path id="primary"
+            d="M20,6.2,13,2.26a2,2,0,0,0-2,0L4,6.19A2,2,0,0,0,3,7.94v8.12A2,2,0,0,0,4,17.8l7,3.94a2,2,0,0,0,2,0l7-3.93a2,2,0,0,0,1-1.75V7.94A2,2,0,0,0,20,6.2ZM12,15a3,3,0,1,1,3-3A3,3,0,0,1,12,15Z"
+            style="fill: #fff;"></path>
+    </svg>
+    <svg fill="#000000" width="15px" height="15px" class="absolute top-[10px] left-[600px] z-10" viewBox="0 0 24 24"
+        id="nut-2" data-name="Flat Color" xmlns="http://www.w3.org/2000/svg" class="icon flat-color">
+        <path id="primary"
+            d="M20,6.2,13,2.26a2,2,0,0,0-2,0L4,6.19A2,2,0,0,0,3,7.94v8.12A2,2,0,0,0,4,17.8l7,3.94a2,2,0,0,0,2,0l7-3.93a2,2,0,0,0,1-1.75V7.94A2,2,0,0,0,20,6.2ZM12,15a3,3,0,1,1,3-3A3,3,0,0,1,12,15Z"
+            style="fill: #fff;"></path>
+    </svg>
+    <svg fill="#000000" width="15px" height="15px" class="absolute top-[55px] left-[600px] z-10" viewBox="0 0 24 24"
+        id="nut-2" data-name="Flat Color" xmlns="http://www.w3.org/2000/svg" class="icon flat-color">
+        <path id="primary"
+            d="M20,6.2,13,2.26a2,2,0,0,0-2,0L4,6.19A2,2,0,0,0,3,7.94v8.12A2,2,0,0,0,4,17.8l7,3.94a2,2,0,0,0,2,0l7-3.93a2,2,0,0,0,1-1.75V7.94A2,2,0,0,0,20,6.2ZM12,15a3,3,0,1,1,3-3A3,3,0,0,1,12,15Z"
+            style="fill: #fff;"></path>
+    </svg>
+    <img src="includes/Icon.png" class="h-20 w-20 absolute top-[25px] left-[70px] z-10" alt="">
+</nav>
+<!-- Full Menu (visible when viewport > 1022px) -->
+<div id="fullMenu" class="hidden lg:flex items-center justify-center gap-[30px] absolute top-[25px] right-10">
+    <a href=""
+        class="text-[#262626] hover:text-[#ff0302] text-lg font-medium relative transition-colors duration-300 group">
+        Home
+    </a>
+    <a href=""
+        class="text-[#262626] hover:text-[#ff0302] text-lg font-medium relative transition-colors duration-300 group">
+        About Us
+    </a>
+    <a href=""
+        class="text-[#262626] hover:text-[#ff0302] text-lg font-medium relative transition-colors duration-300 group">
+        Events
+    </a>
+    <a href=""
+        class="text-[#262626] hover:text-[#ff0302] text-lg font-medium relative transition-colors duration-300 group">
+        Achievements
+    </a>
+    <a href=""
+        class="text-[#262626] hover:text-[#ff0302] text-lg font-medium relative transition-colors duration-300 group">
+        Contact Us
+    </a>
+</div>
+<div class="lg:hidden absolute top-[13px] right-2 z-20">
+    <!-- Boxed Hamburger Icon -->
+    <div class="bg-[#262626] p-2 rounded-md">
+        <button id="hamburger-btn" class="text-white focus:outline-none">
+            <svg class="w-8 h-8" fill="none" stroke="currentColor" viewBox="0 0 24 24"
+                xmlns="http://www.w3.org/2000/svg">
+                <path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M4 6h16M4 12h16m-7 6h7">
+                </path>
+            </svg>
+        </button>
     </div>
-    <div class="lg:hidden absolute top-[13px] right-2 z-20">
-        <!-- Boxed Hamburger Icon -->
-        <div class="bg-[#262626] p-2 rounded-md">
-            <button id="hamburger-btn" class="text-white focus:outline-none">
-                <svg class="w-8 h-8" fill="none" stroke="currentColor" viewBox="0 0 24 24"
-                    xmlns="http://www.w3.org/2000/svg">
-                    <path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M4 6h16M4 12h16m-7 6h7">
-                    </path>
-                </svg>
-            </button>
-        </div>
 
-        <!-- Mobile Menu -->
-        <div id="mobile-menu" class="hidden absolute top-[60px] right-0 bg-[#262626] shadow-lg rounded-lg p-4 w-48">
-            <a href=""
-                class="block text-white hover:text-[#ff0302] text-lg font-medium py-2 transition-colors duration-300">Home</a>
-            <a href=""
-                class="block text-white hover:text-[#ff0302] text-lg font-medium py-2 transition-colors duration-300">About
-                Us</a>
-            <a href=""
-                class="block text-white hover:text-[#ff0302] text-lg font-medium py-2 transition-colors duration-300">Events</a>
-            <a href=""
-                class="block text-white hover:text-[#ff0302] text-lg font-medium py-2 transition-colors duration-300">Achievements</a>
-            <a href=""
-                class="block text-white hover:text-[#ff0302] text-lg font-medium py-2 transition-colors duration-300">Contact
-                Us</a>
-        </div>
+    <!-- Mobile Menu -->
+    <div id="mobile-menu" class="hidden absolute top-[60px] right-0 bg-[#262626] shadow-lg rounded-lg p-4 w-48">
+        <a href=""
+            class="block text-white hover:text-[#ff0302] text-lg font-medium py-2 transition-colors duration-300">Home</a>
+        <a href=""
+            class="block text-white hover:text-[#ff0302] text-lg font-medium py-2 transition-colors duration-300">About
+            Us</a>
+        <a href=""
+            class="block text-white hover:text-[#ff0302] text-lg font-medium py-2 transition-colors duration-300">Events</a>
+        <a href=""
+            class="block text-white hover:text-[#ff0302] text-lg font-medium py-2 transition-colors duration-300">Achievements</a>
+        <a href=""
+            class="block text-white hover:text-[#ff0302] text-lg font-medium py-2 transition-colors duration-300">Contact
+            Us</a>
     </div>
-</body>
+</div>
 <script>
-document.addEventListener("DOMContentLoaded", function() {
-    document.getElementById("hamburger-btn").addEventListener("click", function() {
-        const menu = document.getElementById("mobile-menu");
-        menu.classList.toggle("hidden");
+    document.addEventListener("DOMContentLoaded", function() {
+        document.getElementById("hamburger-btn").addEventListener("click", function() {
+            const menu = document.getElementById("mobile-menu");
+            menu.classList.toggle("hidden");
+        });
     });
-});
 </script>
 
 </html>
